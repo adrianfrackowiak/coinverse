34e8c35499f0fa1e9af76b7f0e7f868bdee4a4b2
Adrian Frąckowiak <67045837+adrianfrackowiak@users.noreply.github.com>
2022-05-16 07:38:35 +0200

    add site to nextauth configuration

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,7 @@ const prisma = new PrismaClient();
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
+  site: process.env.NEXTAUTH_URL,
   providers: [
     Auth0Provider({
       clientId: process.env.AUTH0_CLIENT_ID,
